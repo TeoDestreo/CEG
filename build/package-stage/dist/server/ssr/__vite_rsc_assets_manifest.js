3b5b05445b44d5c4395056565257f1cd968feb7f
TeoDestreo <advisors@fogler.pro>
2026-07-11 23:55:04 -0700

    f
    f

---
+++ build/package-stage/dist/server/ssr/__vite_rsc_assets_manifest.js
@@ -0,0 +1,48 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-Cw_1cOnq.js\")",
+  "clientReferenceDeps": {
+    "c7fa5cfea253": {
+      "js": [
+        "/assets/index-Cw_1cOnq.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "da06f1d026a2": {
+      "js": [
+        "/assets/layout-segment-context-Cu8FUeRw.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-Cw_1cOnq.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    },
+    "c2747888630f": {
+      "js": [
+        "/assets/link-C01Y4q-S.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-Cw_1cOnq.js",
+        "/assets/framework-XU8gKkFC.js",
+        "/assets/query-BbOc3VB2.js"
+      ],
+      "css": []
+    },
+    "5491e5eeb9e7": {
+      "js": [
+        "/assets/index-Cw_1cOnq.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-XU8gKkFC.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-DWw2GRxK.css"
+      ]
+    }
+  }
+}
